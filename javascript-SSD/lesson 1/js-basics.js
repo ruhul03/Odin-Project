46ab4fe88078e32d9c50a10cc9cef7251eb39d5b
Ruhul Amin <asmruhulamin47@gmail.com>
2026-01-15 11:46:55 +0600

    starting js

---
+++ javascript-SSD/lesson 1/js-basics.js
@@ -0,0 +1,13 @@
+alert('hello');
+
+alert('good job');
+
+4 + 4;
+
+10 * 2;
+
+15 / 3;
+
+document.body.innerHTML = 'Welcome to JavaScript Basics!';
+
+document.body.style.backgroundColor = 'lightblue';
